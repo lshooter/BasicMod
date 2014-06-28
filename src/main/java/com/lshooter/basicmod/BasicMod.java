22b8e8472061bb2ad5f9eb3231067366a7b01928
lshooter <steve.maccabe@blueyonder.co.uk>
2014-06-28 22:43:17 +0100

    Changes in Pahimar's Let's Mod Reboot series episode 4

---
+++ src/main/java/com/lshooter/basicmod/BasicMod.java
@@ -0,0 +1,31 @@
+package com.lshooter.basicmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="BasicMod", name="Basic Mod", version="1.7.2-1.0")
+public class BasicMod
+{
+    @Mod.Instance("BasicMod")
+    public static BasicMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
